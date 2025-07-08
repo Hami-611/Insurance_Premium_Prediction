6859405580e0395415e54017ebf9158cd8b5a8a7
Hamika <hamika.redrowthu@gmail.com>
2025-07-08 22:07:19 +0530

    Final setup for Render deployment

--- insurance/settings.py
+++ insurance/settings.py
@@ -79,7 +79,8 @@ WSGI_APPLICATION = 'insurance.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
-        conn_max_age=600
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
